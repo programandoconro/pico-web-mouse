2a8f2a898d5ed9a0ede3bd36149c9d912678587d
programandoconro <programandoconro@gmail.com>
2024-10-27 13:20:43 +0900

    basic websocket server

---
+++ boot.py
@@ -0,0 +1,17 @@
+# boot.py -- run on boot-up
+from credentials import SSID, PW
+
+
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(SSID, PW)
+        while not sta_if.isconnected():
+            pass
+    print('Connected! Network config:', sta_if.ifconfig())
+    
+print("Connecting to your wifi...")
+do_connect()
