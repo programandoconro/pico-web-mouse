b73cee4f17b9257c7759d49be9ace668ae2b22ba
programandoconro <programandoconro@gmail.com>
2024-11-04 16:57:49 +0900

    improve mouse movements using diff

--- mouse.py
+++ mouse.py
@@ -9,8 +9,8 @@ class Mouse:
     def parse_data(self, data: str) -> tuple:
         arr = data.split()
         try:
-            x = int(data[0]) if arr and len(arr) > 0 else None
-            y = int(data[1]) if arr and len(arr) > 1 else None
+            x = int(arr[0]) if arr and len(arr) > 0 else None
+            y = int(arr[1]) if arr and len(arr) > 1 else None
         except:
             x = 0
             y = 0
